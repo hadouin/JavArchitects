25de10635f35c8050794bc444a08e1f969f09bce
Julie <rasser.julie@gmail.com>
2023-01-06 17:20:52 +0100

    add GameController, GameInternal, WonderFragment

---
+++ src/main/java/fr/isep/javarchitects/WonderFragment.java
@@ -0,0 +1,21 @@
+package fr.isep.javarchitects;
+
+public class WonderFragment {
+
+
+    //attributes
+    private final String image;
+    private final int floorNumber;
+    private boolean triggerEffectFragment;
+
+
+    //constructor
+    public WonderFragment(String image, int floorNumber, boolean triggerEffectFragment) {
+        this.image = image;
+        this.floorNumber = floorNumber;
+        this.triggerEffectFragment = triggerEffectFragment;
+    }
+
+    //members
+
+}
