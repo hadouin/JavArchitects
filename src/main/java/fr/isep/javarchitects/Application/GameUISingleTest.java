527e28d770e55c3dd46bd2fdff1a0e4a53246c60
Hadouin Leroy <hadouinl@gmail.com>
2023-01-08 23:27:03 +0100

    rework splash screen

--- src/main/java/fr/isep/javarchitects/Application/GameUISingleTest.java
+++ src/main/java/fr/isep/javarchitects/Application/GameUISingleTest.java
@@ -1,6 +1,7 @@
 package fr.isep.javarchitects.Application;
 
 import fr.isep.javarchitects.Fenetres.GameUI;
+import fr.isep.javarchitects.components.SplashScreen;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
@@ -8,6 +9,8 @@ public class GameUISingleTest extends Application {
     @Override
     public void start(Stage stage) throws Exception {
         GameUI game1 = new GameUI();
-        game1.show();
+        SplashScreen splashScreen = new SplashScreen(game1::show);
+        splashScreen.show();
+        splashScreen.startLoading();
     }
 }
