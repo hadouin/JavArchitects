2df7036f3720c119a18cd1f1f20392c7aae7630b
Hadouin Leroy <hadouinl@gmail.com>
2023-01-08 19:46:36 +0100

    add resource count + VP

--- src/main/java/fr/isep/javarchitects/model/WonderFragment.java
+++ src/main/java/fr/isep/javarchitects/model/WonderFragment.java
@@ -7,13 +7,19 @@ public class WonderFragment {
     private final String ImagePathBack;
     private final int floorNumber;
     private boolean triggerEffect;
+    private int VP;
+    private int resourceCount;
+    private boolean matchingResources;
 
     //constructor
-    public WonderFragment(String imagePathFront, String imagePathBack, int floorNumber, boolean triggerEffect) {
+    public WonderFragment(String imagePathFront, String imagePathBack, int floorNumber, boolean triggerEffect, int vp, int resourceCount, boolean matchingResources) {
         this.imagePathFront = imagePathFront;
         this.ImagePathBack = imagePathBack;
         this.floorNumber = floorNumber;
         this.triggerEffect = triggerEffect;
+        this.VP = vp;
+        this.resourceCount = resourceCount;
+        this.matchingResources = matchingResources;
     }
 
 }
