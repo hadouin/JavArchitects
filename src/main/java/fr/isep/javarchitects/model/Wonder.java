f452fcce5e95f17d953bac6d051cecd09e81592d
Hadouin Leroy <hadouinl@gmail.com>
2023-01-08 18:59:20 +0100

    alexandria done

--- src/main/java/fr/isep/javarchitects/model/Wonder.java
+++ src/main/java/fr/isep/javarchitects/model/Wonder.java
@@ -1,45 +1,60 @@
 package fr.isep.javarchitects.model;
 
+import java.util.Arrays;
+import java.util.List;
+
 public enum Wonder {
 
-	Alexandrie("Alexandrie", "Alexandrie", //
-			"Prenez la premi�re carte d'une pioche au choix, n'importe o� sur la table, et posez-la devant vous", 0),
+	Alexandrie(0,"Alexandrie", "Alexandrie", //
+			"Prenez la premi�re carte d'une pioche au choix, n'importe o� sur la table, et posez-la devant vous",
+			Arrays.asList(
+					new WonderFragment("images/wonders/alexandrie/piece-front-alexandrie-1.png", "images/wonders/alexandrie/piece-back-alexandrie-1.png", 0, false ),
+					new WonderFragment("images/wonders/alexandrie/piece-front-alexandrie-2.png", "images/wonders/alexandrie/piece-back-alexandrie-2.png", 1, true ),
+					new WonderFragment("images/wonders/alexandrie/piece-front-alexandrie-3.png", "images/wonders/alexandrie/piece-back-alexandrie-3.png", 2, false ),
+					new WonderFragment("images/wonders/alexandrie/piece-front-alexandrie-4.png", "images/wonders/alexandrie/piece-back-alexandrie-4.png", 3, true ),
+					new WonderFragment("images/wonders/alexandrie/piece-front-alexandrie-5.png", "images/wonders/alexandrie/piece-back-alexandrie-5.png", 4, false ),
+					new WonderFragment("images/wonders/alexandrie/piece-front-alexandrie-6.png", "images/wonders/alexandrie/piece-back-alexandrie-6.png", 5, false )
+			)),
 	
-	Halicarnasse("Halicarnasse", "Halicarnasse", //
+	Halicarnasse(1,"Halicarnasse", "Halicarnasse", //
 			"Prenez les 5 premi�res cartes de la pioche � votre gauche ou � votre droite, choisissez-en 1 et posez-la devant vous" //
-			+ "M�langez-les cartes restantes dans leur pioche", 1),
+			+ "M�langez-les cartes restantes dans leur pioche",),
 	
-	Ephese("Ephese", "Eph�se", //
-			"Prenez la premi�re carte de la pioche centrale et posez-la devant vous", 2),
+	Ephese(2,"Ephese", "Eph�se", //
+			"Prenez la premi�re carte de la pioche centrale et posez-la devant vous",),
 	
-	Olympie("Olympie", "Olympie", //
-			"Prenez la premi�re carte de la pioche � votre cauche et de celle � votre droite, et posez-les devant vous", 3),
+	Olympie(3,"Olympie", "Olympie", //
+			"Prenez la premi�re carte de la pioche � votre cauche et de celle � votre droite, et posez-les devant vous",),
 	
-	Babylone("Babylone", "Babylone", //
-			"Choisissez 1 jeton Progr�s parmi les 4 disponibles, et posez-le devant vous", 4),
+	Babylone(4,"Babylone", "Babylone", //
+			"Choisissez 1 jeton Progr�s parmi les 4 disponibles, et posez-le devant vous",),
 	
-	Rhodes("Rhodes", "Rhodes", //
-			"Ajoutez 1 Bouclier � votre total de Boucliers", 5),
+	Rhodes(5,"Rhodes", "Rhodes", //
+			"Ajoutez 1 Bouclier � votre total de Boucliers",),
 	
-	Gizeh("Gizeh", "Gizeh", //
-			"Cette merveille n'a pas d'effet particulier, mais rapporte plus de points de victoire que les autres Merveilles", 6);
-	
+	Gizeh(6,"Gizeh", "Gizeh", //
+			"Cette merveille n'a pas d'effet particulier, mais rapporte plus de points de victoire que les autres Merveilles",);
+
 	// ------------------------------------------------------------------------
 	
 	public final String displayName;
-	
 	public final String frenchName;
-	
 	public final String effectDescription;
 	public final int ID;
+	public List<WonderFragment> wonderFragments;
 	
 	// ------------------------------------------------------------------------
 	
-	Wonder(String displayName, String frenchName, String effectDescription, int id) {
+	Wonder(int id,
+		   String displayName,
+		   String frenchName,
+		   String effectDescription,
+		   List<WonderFragment> wonderFragments) {
 		this.displayName = displayName;
 		this.frenchName = frenchName;
 		this.effectDescription = effectDescription;
 		this.ID = id;
+		this.wonderFragments = wonderFragments;
 	}
 
 
