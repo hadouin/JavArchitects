4ec94392ad372beb74fde8261a57d83586e97d09
Hadouin Leroy <hadouinl@gmail.com>
2023-01-10 09:29:23 +0100

    ref Sprite

--- src/main/java/fr/isep/javarchitects/utils/Sprite.java
+++ src/main/java/fr/isep/javarchitects/utils/Sprite.java
@@ -1,5 +1,10 @@
 package fr.isep.javarchitects.utils;
 
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.image.PixelWriter;
+import javafx.scene.image.WritableImage;
+
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
@@ -31,6 +36,13 @@ public class Sprite {
         return sprite;
     }
 
+    /**
+     * @param xGrid column position in sprite sheet grid
+     * @param yGrid row position in sprite sheet grid
+     * @param xSize grid number of columns
+     * @param ySize grid number of rows
+     * @return the image at the given x,y position in the sprite sheet
+     */
     public BufferedImage getSpriteAutoSize(int xGrid, int yGrid, int xSize, int ySize) {
 
         if (spriteSheet == null) {
@@ -42,6 +54,25 @@ public class Sprite {
         }
 
         return spriteSheet.getSubimage(xGrid * xTileSize, yGrid * yTileSize, xTileSize, yTileSize);
+    }
+
+    /**
+     * @param image buffered image to convert
+     * @return return javaFX Image from bufferedImage
+     */
+    public static Image convertToFxImage(BufferedImage image) {
+        WritableImage wr = null;
+        if (image != null) {
+            wr = new WritableImage(image.getWidth(), image.getHeight());
+            PixelWriter pw = wr.getPixelWriter();
+            for (int x = 0; x < image.getWidth(); x++) {
+                for (int y = 0; y < image.getHeight(); y++) {
+                    pw.setArgb(x, y, image.getRGB(x, y));
+                }
+            }
+        }
+
+        return new ImageView(wr).getImage();
     }
 
 }
